7089e5b1a3cb9d1b6b1149aaacdd2000a13e7bca
Anastasia Slabukho <101647250+an-sla@users.noreply.github.com>
2022-05-05 22:45:54 +0200

    whole_num_last_entry_binsearch — code

---
+++ Lecture 2_07.04.2022/whole_num_first_entry_binsearch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+int first_entry_binsearch(std::vector<int> &data, int key) { // передаём вектор по ссылке! -> иначе происходит копирование и линейный поиск
+    int left = -1, right = data.size(); // инициируем фиктивные элементы
+    while (right > left + 1) { // пока индексы не сошлись...
+        int middle = (left + right) / 2;
+        if (data[middle] < key) { // строго меньше, двигаем правый элемент при равности (отрабатывает вилка else)
+            left = middle;
+        } else {
+            right = middle;
+        }
+    }
+    return right; // возвращаем правый элемент
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> sequence(n);
+    for (size_t i = 0; i < n; ++i) {
+        int temp;
+        std::cin >> temp;
+        sequence[i] = temp;
+    }
+    int queries;
+    int key;
+    std::cin >> queries;
+    for (auto i = 0; i < queries; ++i) {
+        std::cin >> key;
+        int index = first_entry_binsearch(sequence, key);
+        if (index != sequence.size() and sequence[index] == key) {
+            std::cout << "Element in array at index " << index << " as first entry" << std::endl;
+        } else {
+            std::cout << "Element not in array" << std::endl;
+        }
+    }
+    return 0;
+}
