e430303c733741539f560e85beb779b05a20eebe
Anastasia Slabukho <101647250+an-sla@users.noreply.github.com>
2022-05-06 18:40:11 +0200

    sparse_table_1D — code

--- Lecture 3_12.04.22/sparse_table_1D.cpp
+++ Lecture 3_12.04.22/sparse_table_1D.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 
-size_t lg2(size_t n) {
+size_t lg2(size_t n) { // двоичный логарифм от числа, округлённый вниз
     int k = 0;
     while ((1 << k) <= n) {
         k++;
@@ -14,17 +14,17 @@ size_t lg2(size_t n) {
 int main() {
     size_t vect_size;
     std::cin >> vect_size;
-    std::vector<size_t> a;
+    std::vector<size_t> a; // исходный вектор
     for (size_t i = 0; i < vect_size; ++i) {
         size_t temp;
         std::cin >> temp;
         a.push_back(temp);
     }
-    std::vector<std::vector<size_t>> p (lg2(a.size()) + 1, std::vector<size_t>(a.size()));
+    std::vector<std::vector<size_t>> p (lg2(a.size()) + 1, std::vector<size_t>(a.size())); // sparse table, длину каждой строки делаем с прикидкой вверх a.size()
     p[0] = a;
     for (size_t l = 0; l < lg2(a.size()); ++l) {
-        for (size_t i = 0; i + (2 << l) <= a.size(); ++i) {
-            p[l + 1][i] = std::min(p[l][i], p[l][i + (1 << l)]);
+        for (size_t i = 0; i + (2 << l) <= a.size(); ++i) { // побитовый сдвиг на l (2 * 2^l)
+            p[l + 1][i] = std::min(p[l][i], p[l][i + (1 << l)]); // i + log2 (l) (логарифм — старший бит числа)
         }
     }
     int query_num;
